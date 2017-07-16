10d4fec503defb47a902d4acaca9d5e7b6b577b2
konstantin krivlenia <krivlenia@gmail.com>
2017-07-16 12:19:24 +0300

    add test with own assert

---
+++ examples/IsPowerOfTwo/IsPowerOfTwo.cool-asserts.test.js
@@ -0,0 +1,10 @@
+const isPowerOfTwo = require('./isPowerOfTwo');
+const assert = require('./cool-assert');
+
+assert.equal(isPowerOfTwo(0), false, '0 should not be power of two');
+assert.equal(isPowerOfTwo(1), true, '1 should be power of two');
+assert.equal(isPowerOfTwo(2), true, '2 should be power of two');
+assert.equal(isPowerOfTwo(3), true, '3 should not be power of two');
+assert.equal(isPowerOfTwo(6), false, '6 should not be power of two');
+assert.equal(isPowerOfTwo(8), true, '2 should be power of two');
+assert.equal(isPowerOfTwo(-2), false, '-2 should not be power of two');
