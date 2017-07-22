d09a003751b37b39f5ba959417ef0613249dda3b
konstantin krivlenia <krivlenia@gmail.com>
2017-07-22 16:10:31 +0300

    implement video list

--- examples/youtube-app/__tests__/Youtube-api.tests.js
+++ examples/youtube-app/__tests__/Youtube-api.tests.js
@@ -1,30 +1,9 @@
 import YoutubeApi from '../Youtube-api';
 import searchResponse from '../__mocks__/search-response.json';
 import detailsResponse from '../__mocks__/details-response.json';
+import expectedVideos from '../__mocks__/videos.json';
 
 const youtubeApi = new YoutubeApi('mykey', 10);
-const expectedVideos = {
-    items:[
-        {
-            id:"1",
-            title: "Title 1",
-            publishedAt: "2017-02-23T08:41:03.000Z",
-            channelTitle: "channelTitle 1",
-            description: "Video description 1",
-            viewCount: "20",
-            thumbnailUrl: "thumbnailUrl1"
-        },
-        {
-            id:"2",
-            title: "Title 2",
-            publishedAt: "2017-03-23T08:41:03.000Z",
-            channelTitle: "channelTitle 2",
-            description: "Video description 2",
-            viewCount: "10",
-            thumbnailUrl: "thumbnailUrl2"
-        }
-    ]
-};
 
 describe('youtube api', () => {
     describe('get video by search query', function () {
@@ -54,8 +33,8 @@ describe('youtube api', () => {
         });
         it('getVideoByQuery return videos', () => {
             global.fetch = require('jest-fetch-mock');
-            const searchRequest = fetch.mockResponseOnce(JSON.stringify(searchResponse));
-            const detailsRequest =  fetch.mockResponseOnce(JSON.stringify(detailsResponse));
+            fetch.mockResponseOnce(JSON.stringify(searchResponse));
+            fetch.mockResponseOnce(JSON.stringify(detailsResponse));
             return youtubeApi.getVideoByQuery('rolling-scopes-school').then((res) => {
                 expect(fetch).toHaveBeenCalledWith(
                     "https://www.googleapis.com/youtube/v3//videos?key=mykey&id=1,2&part=statistics"
