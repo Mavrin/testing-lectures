5167dc1d1b9f6bde55fe9fcf6be1c30b94f7890d
konstantin krivlenia <krivlenia@gmail.com>
2017-07-22 14:55:43 +0300

    Implement youtube api

---
+++ examples/youtube-app/Youtube-api.js
@@ -0,0 +1,45 @@
+export default class YoutubeApi {
+    constructor(key, count) {
+        this.key = key;
+        this.apiUrl = 'https://www.googleapis.com/youtube/v3/';
+        this.count = count;
+    }
+    getSearchQueryUrl(query) {
+        return `${this.apiUrl}search?key=${this.key}&type=video&part=snippet&maxResults=${this.count}&q=${query}`;
+    }
+    extractIds(response) {
+        return response.items.map(({id:{videoId}}) => videoId);
+    }
+    getDetailsUrl(ids) {
+        return `${this.apiUrl}/videos?key=${this.key}&id=${ids.join(',')}&part=statistics`;
+    }
+    mergeResult(searchResult, detailsResult) {
+        return searchResult.items.reduce((res,item) => {
+            const id = item.id.videoId;
+            const {title, publishedAt, channelTitle, description, thumbnails} = item.snippet;
+            const detail = detailsResult.items.find(({id: videoId}) => videoId === id);
+            res.items.push({
+                id,
+                title,
+                publishedAt,
+                channelTitle,
+                description,
+                viewCount: detail.statistics.viewCount,
+                thumbnailUrl: thumbnails.medium.url,
+            });
+            return res;
+        },{items:[]})
+    }
+    getRequest(url) {
+        return fetch(url).then(res => res.json())
+    }
+    getVideoByQuery(query) {
+        return this.getRequest(this.getSearchQueryUrl(query))
+            .then((res) => Promise.all([
+                res,
+                this.getRequest(this.getDetailsUrl(this.extractIds(res)))
+            ]))
+            .then(([searchResult, detailsResult]) => this.mergeResult(searchResult, detailsResult))
+
+    }
+}
---
+++ examples/youtube-app/__tests__/Youtube-api.tests.js
@@ -0,0 +1,70 @@
+import YoutubeApi from '../Youtube-api';
+import searchResponse from '../__mocks__/search-response.json';
+import detailsResponse from '../__mocks__/details-response.json';
+
+const youtubeApi = new YoutubeApi('mykey', 10);
+const expectedVideos = {
+    items:[
+        {
+            id:"1",
+            title: "Title 1",
+            publishedAt: "2017-02-23T08:41:03.000Z",
+            channelTitle: "channelTitle 1",
+            description: "Video description 1",
+            viewCount: "20",
+            thumbnailUrl: "thumbnailUrl1"
+        },
+        {
+            id:"2",
+            title: "Title 2",
+            publishedAt: "2017-03-23T08:41:03.000Z",
+            channelTitle: "channelTitle 2",
+            description: "Video description 2",
+            viewCount: "10",
+            thumbnailUrl: "thumbnailUrl2"
+        }
+    ]
+};
+
+describe('youtube api', () => {
+    describe('get video by search query', function () {
+        it('should generate the search url', () => {
+            expect(
+                youtubeApi.getSearchQueryUrl('rolling-scopes-school')
+            )
+                .toEqual('https://www.googleapis.com/youtube/v3/search?key=mykey&type=video&part=snippet&maxResults=10&q=rolling-scopes-school')
+        });
+        it('should extract ids', () => {
+            expect(
+                youtubeApi.extractIds(searchResponse)
+            )
+                .toEqual(['1', '2']);
+        });
+        it('should generate the details url', () => {
+            expect(
+                youtubeApi.getDetailsUrl(['1', '2'])
+            )
+                .toEqual('https://www.googleapis.com/youtube/v3//videos?key=mykey&id=1,2&part=statistics');
+        });
+        it('should merge search and details result', () => {
+            expect(
+                youtubeApi.mergeResult(searchResponse, detailsResponse)
+            )
+                .toEqual(expectedVideos);
+        });
+        it('getVideoByQuery return videos', () => {
+            global.fetch = require('jest-fetch-mock');
+            const searchRequest = fetch.mockResponseOnce(JSON.stringify(searchResponse));
+            const detailsRequest =  fetch.mockResponseOnce(JSON.stringify(detailsResponse));
+            return youtubeApi.getVideoByQuery('rolling-scopes-school').then((res) => {
+                expect(fetch).toHaveBeenCalledWith(
+                    "https://www.googleapis.com/youtube/v3//videos?key=mykey&id=1,2&part=statistics"
+                );
+                expect(fetch).toHaveBeenCalledWith(
+                    "https://www.googleapis.com/youtube/v3/search?key=mykey&type=video&part=snippet&maxResults=10&q=rolling-scopes-school"
+                );
+                return expect(res).toEqual(expectedVideos);
+            });
+        })
+    })
+});
